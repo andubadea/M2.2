bf0d232433643a594af1f8efe0e7f8cf23d3d5b7
andubadea <37139260+andubadea@users.noreply.github.com>
2023-07-14 11:17:52 +0200

    Coins

---
+++ streets_process.py
@@ -0,0 +1,27 @@
+import geopandas as gpd
+import momepy
+import osmnx as ox
+
+# Select city
+city = 'Vienna'
+
+# Load graphml
+G = ox.load_graphml(city + '/streets.graphml')
+
+# Get the nodes, edges
+nodes, edges = ox.graph_to_gdfs(G)
+
+# Run coins
+continuity = momepy.COINS(edges, angle_threshold=90)
+edges['stroke'] = continuity.stroke_attribute()
+
+
+# Repackage
+G_coined = ox.graph_from_gdfs(nodes, edges)
+
+# Save
+ox.save_graphml(G_coined, city + '/streets_coined.graphml')
+ox.save_graph_geopackage(G_coined, city + '/streets_coined.gpkg')
+
+
+
